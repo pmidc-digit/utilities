ac2016b93a124d7894663c34bc63da87ef8ae20d
Priyanka-eGov <priyanka@egovernments.org>
2021-08-18 16:47:54 +0530

    Adding API call to property tax script

--- datamart/property-tax/pt.py
+++ datamart/property-tax/pt.py
@@ -1,8 +1,9 @@
-    
 import psycopg2
 import csv
 import pandas as pd
 import numpy as np
+import requests
+import json
 
 def map_MC(s):
     if s in MC:
@@ -188,6 +189,29 @@ NP = ['pb.ajnala',
                	'pb.talwandisabo',
                	'pb.talwara']
 
+def map_subownership(s):
+    if s == 'Multipleowners':
+        return 'Multiple Owners'
+    elif s == 'Singleowner':
+        return 'Single Owner'
+    elif s == 'Privatetrust':
+        return 'Private Trust'
+    elif s == 'Privatecompany':
+        return 'Private Company'
+    elif s == 'Othersprivateinstituition':
+        return 'Others Private Instituition'
+    elif s == 'Stategovernment':
+        return 'State Government'
+    elif s == 'Othergovernmentinstituition':
+        return 'Other Government Instituition'
+    elif s == 'Privateboard':
+        return 'Private Board'
+    elif s == 'Central Government':
+        return 'Central Government'
+    elif s == 'Ulbgovernment':
+        return 'Ulb Government'
+    elif s == 'Ngo':
+        return 'Ngo'
 #Replace with correct password in relation to the database
 
 def connect():
@@ -200,8 +224,8 @@ def connect():
         print("Exception occurred while connecting to the database")
         print(exception)
     
-    property_detailsquery = pd.read_sql_query("select * from eg_pt_property where status='ACTIVE' and createdtime < 1606780799000 AND tenantId != 'pb.testing'", conn)
-    property_changesquery = pd.read_sql_query("SELECT ptid, isUsageCategorychanged,isOwnerShipCategorychanged,isOwnerDetailChanged,isUnitAreaChanged,financialyears,numberOfAssessments FROM ( SELECT data_rank.ptid as ptid, MAX(usage_rank) > 1 AS isUsageCategoryChanged, MAX(ownercategory_rank) > 1 AS isOwnerShipCategoryChanged,          MAX(ownerdetail_rank) > 1 AS isOwnerDetailChanged,         MAX(unitarea_rank) > 1 AS isUnitAreaChanged FROM ( SELECT             *, RANK() OVER( PARTITION BY pt_data.ptid ORDER BY pt_data.usagemajor DESC ) usage_rank , RANK() OVER( PARTITION BY pt_data.ptid ORDER BY pt_data.ownercategory DESC ) ownercategory_rank, RANK() OVER( PARTITION BY pt_data.ptid ORDER BY pt_data.ownerids DESC ) ownerdetail_rank, RANK() OVER (PARTITION BY pt_data.ptid ORDER BY pt_data.totalunitarea DESC) unitarea_rank FROM (SELECT *, pt.propertyid AS ptid FROM eg_pt_property_v2 pt INNER JOIN (( SELECT *, CONCAT(usagecategorymajor, ',', usagecategoryminor) AS usagemajor, CONCAT(ownershipcategory, ',', subownershipcategory) AS ownercategory FROM eg_pt_propertydetail_v2 pd LEFT OUTER JOIN ( select SUM(unitarea) as totalunitarea, propertydetail from eg_pt_unit_v2 GROUP BY propertydetail) ut ON ut.propertydetail = pd.assessmentnumber WHERE tenantid != 'pb.testing' AND createdtime < 1606780799000 AND createdtime IN( SELECT max(createdtime) FROM eg_pt_propertydetail_v2 GROUP BY property, financialyear ) ) tmp INNER JOIN ( SELECT ARRAY_AGG( ow.userid ORDER BY ow.userid ) ownerids, propertydetail from eg_pt_owner_v2 ow GROUP BY propertydetail) owner_data ON owner_data.propertydetail = tmp.assessmentnumber)epd ON epd.property = pt.propertyid) pt_data ORDER BY usage_rank DESC ) data_rank GROUP BY data_rank.ptid ) fin_data INNER JOIN ( SELECT ARRAY_AGG( pd.financialyear ORDER BY pd.financialyear ) financialyears,property, count(DISTINCT financialyear) as numberOfAssessments from eg_pt_propertydetail_v2 pd WHERE tenantid != 'pb.testing' group by property) asmt_count ON  asmt_count.property = fin_data.ptid", conn)
+    property_detailsquery = pd.read_sql_query("select eg_pt_property.*, adr.locality as locality from eg_pt_property left outer join eg_pt_address adr on eg_pt_property.id = adr.propertyid where eg_pt_property.status='ACTIVE' and eg_pt_property.createdtime < 1606780799000 AND eg_pt_property.tenantId != 'pb.testing'", conn)
+    property_changesquery = pd.read_sql_query("SELECT ptid, isUsageCategorychanged,isOwnerShipCategorychanged,isOwnerDetailChanged,isUnitAreaChanged,financialyears,numberOfAssessments FROM ( SELECT data_rank.ptid as ptid, MAX(usage_rank) > 1 AS isUsageCategoryChanged, MAX(ownercategory_rank) > 1 AS isOwnerShipCategoryChanged,          MAX(ownerdetail_rank) > 1 AS isOwnerDetailChanged,         MAX(unitarea_rank) > 1 AS isUnitAreaChanged FROM ( SELECT             *, RANK() OVER( PARTITION BY pt_data.ptid ORDER BY          pt_data.usagemajor DESC ) usage_rank , RANK() OVER( PARTITION BY pt_data.ptid ORDER BY pt_data.ownercategory DESC ) ownercategory_rank, RANK() OVER( PARTITION BY pt_data.ptid ORDER BY pt_data.ownerids DESC ) ownerdetail_rank, RANK() OVER (PARTITION BY pt_data.ptid ORDER BY pt_data.totalunitarea DESC) unitarea_rank FROM (SELECT *, pt.propertyid AS ptid FROM eg_pt_property_v2 pt INNER JOIN (( SELECT *, CONCAT(usagecategorymajor, ',', usagecategoryminor) AS usagemajor, CONCAT(ownershipcategory, ',', subownershipcategory) AS ownercategory FROM eg_pt_propertydetail_v2 pd LEFT OUTER JOIN ( select SUM(unitarea) as totalunitarea, propertydetail from eg_pt_unit_v2 GROUP BY propertydetail) ut ON ut.propertydetail = pd.assessmentnumber WHERE tenantid != 'pb.testing' AND createdtime < 1606780799000 AND createdtime IN( SELECT max(createdtime) FROM eg_pt_propertydetail_v2 GROUP BY property, financialyear ) ) tmp INNER JOIN ( SELECT ARRAY_AGG( ow.userid ORDER BY ow.userid ) ownerids, propertydetail from eg_pt_owner_v2 ow GROUP BY propertydetail) owner_data ON owner_data.propertydetail = tmp.assessmentnumber)epd ON epd.property = pt.propertyid) pt_data ORDER BY usage_rank DESC ) data_rank GROUP BY data_rank.ptid ) fin_data INNER JOIN ( SELECT ARRAY_AGG( pd.financialyear ORDER BY pd.financialyear ) financialyears,property, count(DISTINCT financialyear) as numberOfAssessments from eg_pt_propertydetail_v2 pd WHERE tenantid != 'pb.testing' group by property) asmt_count ON  asmt_count.property = fin_data.ptid", conn)
     property_paymentsquery = pd.read_sql_query("SELECT eb.consumercode, count(*) as numberOfPayments, SUM(ep.totalamountpaid) FROM egcl_payment ep INNER JOIN egcl_paymentdetail epd ON ep.id=epd.paymentid INNER JOIN egcl_bill eb ON eb.id=epd.billid INNER JOIN (SELECT DISTINCT(propertyid) FROM eg_pt_property_v2 WHERE status ='ACTIVE' AND tenantId != 'pb.testing') pt ON pt.propertyid=eb.consumercode WHERE ep.createdtime < 1606780799000 AND paymentStatus!='CANCELLED' GROUP BY eb.consumercode", conn)
     property_demandsquery = pd.read_sql_query("select consumercode,sum(pt_tax) as total_tax_due from (select pt_tax,consumercode from egbs_demand_v1 dd INNER JOIN ( select SUM(taxamount) as pt_tax, demandid from egbs_demanddetail_v1 GROUP BY demandid ) ddtl ON dd.id = ddtl.demandid where dd.id IN ( select id from egbs_demand_v1 dd WHERE dd.tenantId!='pb.testing' AND businessservice = 'PT' AND dd.status='ACTIVE' AND dd.createdtime < 1606780799000) ) tp GROUP BY consumercode", conn)
     user_typesquery = pd.read_sql_query("select uuid,type,gender from eg_user", conn)
@@ -233,20 +257,139 @@ def connect():
 
     columns_to_retain=['propertyid', 'tenantid','propertytype',
        'ownershipcategory', 'usagecategory', 'creationreason', 'nooffloors',
-       'landarea', 'superbuiltuparea', 'source', 'channel',
-        'createdby', 'lastmodifiedby', 'createdtime', 'lastmodifiedtime','additionaldetails',
+       'landarea', 'superbuiltuparea', 'source', 'channel', 'additionaldetails',
        'isusagecategorychanged','isownershipcategorychanged', 'isownerdetailchanged',
        'isunitareachanged', 'financialyears', 'numberofassessments','numberofpayments',
        'sum','total_tax_due','type','gender','propertySubType','subownershipCategory',
-        'usageCategoryMinor','ULB_Type']
-
+        'usageCategoryMinor','ULB_Type','locality']
+    
     data = data[columns_to_retain]
+    global uniquetenant
+    uniquetenant = data['tenantid'].unique()
+    global accesstoken 
+    accesstoken = accessToken()
+    global localitydict
+    localitydict={}
+    storeTenantValues()    
+
+    data['Locality'] = data.apply(lambda x : enrichLocality(x.tenantid,x.locality), axis=1)
+    data['Locality'] = data['Locality'].str.upper().str.title()
+    data['City'] = data['tenantid'].apply(lambda x: x[3:])
+    data['City']=data['City'].str.upper().str.title()
+    data['State'] = data['tenantid'].apply(lambda x: 'Punjab' if x[0:2]=='pb' else '')
+
+    data = data.drop(columns=['tenantid','locality'])
     dict = {0 : "Others", 1 : "Female", 2 : "Male", 3 : "Transgender"}
     data['gender'] = data['gender'].map(dict)
-    data = data.rename(columns={"sum": "Total_Amount_Paid","type":"UserTypeOfCreator"})
+    
+    data = data.rename(columns={"sum": "Total_Amount_Paid","type":"User Type Of Creator",
+                           "propertyid": "Property Id",
+                            "propertytype" : "Property Type",
+                            "ownershipcategory":"Ownership Category",
+                            "usagecategory":"Usage Category",
+                            "creationreason":"Creation Reason",
+                            "landarea":"Land Area",
+                            "superbuiltuparea":"Builtup Area",
+                            "source":"Source",
+                            "channel":"Channel",
+                           "additionaldetails":"Additional Details",
+                               "isusagecategorychanged":"Usage Category Changed?",
+                               "isownershipcategorychanged":"Ownership Category Changed?",
+                               "isownerdetailchanged":"Owner Detail Changed?",
+                               "isunitareachanged":"Unit Area Changed?",
+                               "financialyears":"Financial Years",
+                               "numberofassessments":"Number of Assessments",
+                               "numberofpayments":"Number of Payments",
+                            "sum":"Sum",
+                               "total_tax_due":"Total Tax Due",
+                               "type":"Type",
+                            "gender":"Gender",
+                               "propertySubType":"Property Subtype",
+                               "subownershipCategory":"Subownership Category",
+                              "usageCategoryMinor":"Usage Category Minor",
+                               "ULB_Type":"ULB Type"                      })
+    
+    data["Property Type"]= data["Property Type"].str.upper().str.title()
+    data["Ownership Category"]= data["Ownership Category"].str.upper().str.title()
+    data["Usage Category"]= data["Usage Category"].str.upper().str.title()
+    data["Creation Reason"]= data["Creation Reason"].str.upper().str.title()
+    data["Source"]= data["Source"].str.upper().str.title()
+    data["Channel"]= data["Channel"].str.upper().str.title()
+    data["Type"]= data["Type"].str.upper().str.title()
+    data["Property Subtype"]= data["Property Subtype"].str.upper().str.title()
+    data["Subownership Category"]= data["Subownership Category"].str.upper().str.title()
+    data["Usage Category Minor"]= data["Usage Category Minor"].str.upper().str.title()
+    
+    data['Usage Category Changed?']=data['Usage Category Changed?'].map({False:'No',True:'Yes'})
+    data['Ownership Category Changed?']=data['Ownership Category Changed?'].map({False:'No',True:'Yes'})
+    data['Owner Detail Changed?']=data['Owner Detail Changed?'].map({False:'No',True:'Yes'})
+    data['Unit Area Changed?']=data['Unit Area Changed?'].map({False:'No',True:'Yes'})
+    data['Source']=data['Source'].map({'Municipal_Records':'Municipal Records'})
+    data['Property Subtype']=data['Property Subtype'].map({'Independentproperty':'Independent Property','Sharedproperty':'Shared Property'})
+    
+    data['Subownership Category'] = data['Subownership Category'].map(map_subownership)
+    data.fillna("", inplace=True)
     data.to_csv('/tmp/ptDatamart.csv')
     print("Datamart exported. Please copy it using kubectl cp command to you required location.")
+
+def accessToken():
+    query = {'username':'{{REPLACE-WITH-USERNAME}}','password':'{{REPLACE-WITH-PASSWORD}}','userType':'EMPLOYEE',"scope":"read","grant_type":"password"}
+    query['tenantId']='pb.amritsar'
+    response = requests.post("{{REPLACE-WITH-URL}}",data=query, headers={
+   "Connection":"keep-alive","content-type":"application/x-www-form-urlencoded", "origin":"{{REPLACE-WITH-URL}}","Authorization": "Basic ZWdvdi11c2VyLWNsaWVudDo="})
+    jsondata = response.json()
+    return jsondata.get('access_token')
+
+
+def locationApiCall(tenantid):
+    body = { "RequestInfo": {"apiId": "Rainmaker", "ver": ".01","ts": "","action": "","did": "1","key": "","msgId": "20170310130900|en_IN",}}
+    body["RequestInfo"]["authToken"]=accesstoken
+    paramlist = {"hierarchyTypeCode":"REVENUE","boundaryType":"locality"}
+    paramlist["tenantId"]=tenantid
+    response = requests.post("{{REPLACE-WITH-URL}}",params = paramlist,json=body, headers={
+       "Connection":"keep-alive","content-type":"application/json;charset=UTF-8", "origin":"{{REPLACE-WITH-URL}}"})
+
+    jsondata={}
+    if response.status_code == 200:
+        jsondata = response.json()
+    else:
+        return ''
+
+    if 'TenantBoundary' in jsondata:
+        jsondata = jsondata['TenantBoundary']
+    else:
+        return ''
+    if len(jsondata)>0:
+        jsondata = jsondata[0]
+    else:
+        return ''    
+    if 'boundary' in jsondata:
+        jsondata = jsondata['boundary']
+    else:
+        return '' 
     
+    dictionary={} 
+    for v in jsondata:
+        dictionary[v['code']]= v['name']
+            
+    return dictionary     
+    
+def storeTenantValues():
+    for tenant in uniquetenant:
+        localitydict[tenant]=locationApiCall(tenant)
+
+       
+def enrichLocality(tenantid,locality):
+    if tenantid in localitydict:
+        if localitydict[tenantid]=='':
+            return ''
+        elif locality in localitydict[tenantid]:
+            return localitydict[tenantid][locality]
+        else:
+            return ''
+    else:
+        return ''    
+        
+        
 if __name__ == '__main__':
     connect()
-
