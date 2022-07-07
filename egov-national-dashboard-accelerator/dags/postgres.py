1a52a17daab8201345c061222a664e1833d9e66a
anika-eGov <anika.mehrotra@governments.org>
2022-07-07 13:35:42 +0530

    postgres db dag

--- egov-national-dashboard-accelerator/dags/postgres.py
+++ egov-national-dashboard-accelerator/dags/postgres.py
@@ -1,33 +1,24 @@
 import airflow
+import logging
 from datetime import timedelta
 from airflow import DAG
 from airflow.operators.postgres_operator import PostgresOperator
 from airflow.utils.dates import days_ago
 
-args={'owner': 'airflow'}
 
 default_args = {
-    'owner': 'airflow',    
-    #'start_date': airflow.utils.dates.days_ago(2),
-    # 'end_date': datetime(),
-    # 'depends_on_past': False,
-    #'email': ['airflow@example.com'],
-    #'email_on_failure': False,
-    # 'email_on_retry': False,
-    # If a task fails, retry it once after waiting
-    # at least 5 minutes
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(seconds=10),
+    'start_date': datetime(2017, 1, 24)
+
 }
 
 dag_psql = DAG(
     dag_id = "postgresoperator_demo",
-    default_args=args,
-    # schedule_interval='0 0 * * *',
-    schedule_interval='@once',	
-    dagrun_timeout=timedelta(minutes=60),
-    description='use case of psql operator in airflow',
-    start_date = airflow.utils.dates.days_ago(1)
+    default_args=default_args,
+    schedule_interval=None
 )
 
 
