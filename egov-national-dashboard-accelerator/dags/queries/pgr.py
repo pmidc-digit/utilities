06af6efe45104fc0c66f43bd2ec48792bd430ae4
anika-eGov <anika.mehrotra@governments.org>
2022-08-04 16:46:36 +0530

    adding latest files for the dag

--- egov-national-dashboard-accelerator/dags/queries/pgr.py
+++ egov-national-dashboard-accelerator/dags/queries/pgr.py
@@ -405,10 +405,7 @@ def extract_pgr_completion_rate(metrics, region_bucket):
   for department_bucket in department_buckets:
     department_name = department_bucket.get('key')
     value = 0
-    sla_agg = department_bucket.get('all_matching_docs').get('buckets').get('all')
-    logging.info(sla_agg.get('completionRate').get('value'))
-    value = sla_agg.get('completionRate').get('value') if sla_agg.get('completionRate')  else 0
-
+    value = department_bucket.get('all_matching_docs').get('buckets').get('all').get('completionRate').get('value')
     completionRate['buckets'].append( { 'name' : department_name, 'value' :value})
 
   metrics['completionRate'] = [completionRate]
