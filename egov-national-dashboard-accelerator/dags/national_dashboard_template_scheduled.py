c7e431871932122d4a59172132aa8dcb09e3c05a
anika-eGov <anika.mehrotra@governments.org>
2022-08-17 14:54:46 +0530

    adding scheduler

--- egov-national-dashboard-accelerator/dags/national_dashboard_template_scheduled.py
+++ egov-national-dashboard-accelerator/dags/national_dashboard_template_scheduled.py
@@ -48,7 +48,7 @@ module_map = {
 }
 
 
-dag = DAG('national_dashboard_template_scheduled', catchup = True, default_args=default_args, schedule_interval='@hourly')
+dag = DAG('national_dashboard_template_scheduled', catchup = True, default_args=default_args, schedule_interval='@daily')
 log_endpoint = 'kibana/api/console/proxy'
 batch_size = 50
 
