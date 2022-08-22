7ae4f8c592b74bbc2af40de8d5dc3618b8096433
anika-eGov <anika.mehrotra@governments.org>
2022-08-22 20:00:20 +0530

    add revax

--- egov-national-dashboard-accelerator/dags/national_dashboard_template_latest.py
+++ egov-national-dashboard-accelerator/dags/national_dashboard_template_latest.py
@@ -500,7 +500,7 @@ transform_common = PythonOperator(
     dag=dag)
 
 load_common = PythonOperator(
-    task_id='nudb_ingest_load_common',
+    task_id='test',
     python_callable=elastic_dump_pt,
     provide_context=True,
     dag=dag)
