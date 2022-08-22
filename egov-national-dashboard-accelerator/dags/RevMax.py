6b4ed4357b5bb80e7062f117a3dc3037a9ccdf61
anika-eGov <anika.mehrotra@governments.org>
2022-08-22 22:31:43 +0530

    add revax

--- egov-national-dashboard-accelerator/dags/RevMax.py
+++ egov-national-dashboard-accelerator/dags/RevMax.py
@@ -15,17 +15,44 @@ default_args = {
 'retries': 3,
 'retry_delay': timedelta(seconds=10),
 'start_date': datetime(2017, 1, 24)
-
 }
 
 dag = DAG('rev_max', default_args=default_args, schedule_interval=None)
 
-def test():
-    logging.info('testing DAG')
+property_details = {
+  'path': 'property-services/_search',
+  'name': 'property_details',
+  'query': """
+{{
+  "size":1000,
+    "query": {{
+        "bool": {{
+          "must_not": [
+            {{
+              "term": {{
+                "Data.tenantId.keyword": "pb.testing"
+              }}
+            }}
+          ]
+        }}
+      }}
+}}
+
+    """
+}  
+
+def elastic_dump_pt():
+    hook = ElasticHook('GET', 'es_conn')
+    resp = hook.search('property-services/_search', json.loads(property_details('query')))
+    logging.info(resp)
+    logging.info(resp['hits']['hits'])
+    with open("/opt/airflow/dags/json/property_service.json", "w") as outfile:
+        json.dump(elastic_dump_pt, outfile)
+    return resp['hits']['hits']
 
 flatten_data = PythonOperator(
 task_id='flatten_data',
-python_callable=test,
+python_callable=elastic_dump_pt,
 provide_context=True,
 dag=dag)
 
