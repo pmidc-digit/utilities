8a77ecfaaaf822d3ac475999b3dbc341d35b9e56
anika-eGov <anika.mehrotra@governments.org>
2022-08-23 07:01:37 +0530

    add revax

--- egov-national-dashboard-accelerator/dags/RevMax.py
+++ egov-national-dashboard-accelerator/dags/RevMax.py
@@ -19,32 +19,74 @@ default_args = {
 }
 
 dag = DAG('rev_max', default_args=default_args, schedule_interval=None)
-
-property_details = {
-  'path': 'property-services/_search',
-  'name': 'property_details',
-  'query': """
-{{
-  "size":1000,
-    "query": {{
-        "bool": {{
-          "must_not": [
-            {{
-              "term": {{
-                "Data.tenantId.keyword": "pb.testing"
-              }}
-            }}
-          ]
-        }}
-      }}
-}}
-
-    """
-}  
-
+ 
 def elastic_dump_pt():
     hook = ElasticHook('GET', 'es_conn')
     resp = hook.search('property-services/_search', {
+    "size": 10,
+    "_source": ["Data.propertyId","data.superBuiltUpArea","Data.channel", "Data.tenantId", "Data.ward.name", 
+    "Data.ward.code","Data.source", "Data.propertyType", "Data.accountId", "Data.noOfFloors", "Data.@timestamp", 
+    "Data.ownershipCategory", "Data.acknowldgementNumber", "Data.usageCategory", "Data.status"],
+    "query": {
+        "bool": {
+        "must_not": [
+            {
+            "term": {
+                "Data.tenantId.keyword": "pb.testing"
+            }
+            }
+        ]
+        }
+    },
+    "sort": [
+        {
+        "Data.@timestamp": {
+            "order": "desc"
+        }
+        }
+    ]
+    }
+    )
+    logging.info(resp['hits']['hits'])
+    with open("property_service.json", "w") as outfile:
+        outfile.write(resp['hits']['hits'])
+    return resp['hits']['hits']
+
+def elastic_dump_tl():
+    hook = ElasticHook('GET', 'es_conn')
+    resp = hook.search('tlindex-v1-enriched/_search', {
+    "size": 10000,
+    "_source": [
+    "Data.ward.name",
+    "Data.ward.code",
+    "Data.tradelicense"
+  ],
+  "query": {
+    "bool": {
+      "must_not": [
+        {
+          "term": {
+            "Data.tradelicense.tenantId.keyword": "pb.testing"
+          }
+        }
+      ]
+    }
+  },
+  "sort": [
+    {
+      "Data.tradelicense.@timestamp": {
+        "order": "desc"
+      }
+    }
+  ]
+    })
+    logging.info(resp)
+    logging.info(resp['hits']['hits'])
+    return resp['hits']['hits']
+
+def elastic_dump_ws():
+    hook = ElasticHook('GET', 'es_conn')
+    resp = hook.search('wsapplications/_search', {
         "size": 10,
         "query": {
         "match_all": {}
@@ -59,8 +101,45 @@ def elastic_dump_pt():
     })
     logging.info(resp)
     logging.info(resp['hits']['hits'])
-    with open("property_service.json", "w") as outfile:
-        json.dump(elastic_dump_pt, outfile)
+    return resp['hits']['hits']
+
+def elastic_dump_collection_pt():
+    hook = ElasticHook('GET', 'es_conn')
+    resp = hook.search('dss-collection_v2/_search', {
+        "size": 10,
+        "query": {
+        "match_all": {}
+         },
+        "sort": [
+        {
+        "Data.@timestamp": {
+            "order": "desc"
+        }
+        }
+    ]
+    })
+    logging.info(resp)
+    logging.info(resp['hits']['hits'])
+    return resp['hits']['hits']
+
+
+def elastic_dump_meter():
+    hook = ElasticHook('GET', 'es_conn')
+    resp = hook.search('meter-services/_search', {
+        "size": 10,
+        "query": {
+        "match_all": {}
+         },
+        "sort": [
+        {
+        "Data.currentReadingDate": {
+            "order": "desc"
+        }
+        }
+    ]
+    })
+    logging.info(resp)
+    logging.info(resp['hits']['hits'])
     return resp['hits']['hits']
 
 flatten_data = PythonOperator(
