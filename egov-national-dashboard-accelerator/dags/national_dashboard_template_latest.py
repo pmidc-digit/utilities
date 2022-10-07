5153346725a4281f7ffc0ed1525e599666e00df5
jitendra-eGov <100871838+jitendra-eGov@users.noreply.github.com>
2022-10-07 12:54:57 +0530

    Added the replace logic for special character

--- egov-national-dashboard-accelerator/dags/national_dashboard_template_latest.py
+++ egov-national-dashboard-accelerator/dags/national_dashboard_template_latest.py
@@ -211,6 +211,8 @@ def transform_single(single_document, ward_map, date, lambda_function, module):
     ward_buckets = ward_agg.get('buckets')
     for ward_bucket in ward_buckets:
         ward = ward_bucket.get('key')
+        logging.info("ward info")
+        logging.info(ward)
         ulb_agg = ward_bucket.get('ulb')
         ulb_buckets = ulb_agg.get('buckets')
         for ulb_bucket in ulb_buckets:
