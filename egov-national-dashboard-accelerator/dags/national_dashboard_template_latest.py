2e85784212ae774ef2f727ba31cb2f1a1e0743b3
jitendra-eGov <100871838+jitendra-eGov@users.noreply.github.com>
2022-11-15 23:24:29 +0530

    Updated the logs for region buckets

--- egov-national-dashboard-accelerator/dags/national_dashboard_template_latest.py
+++ egov-national-dashboard-accelerator/dags/national_dashboard_template_latest.py
@@ -209,8 +209,7 @@ def transform_single(single_document, ward_map, date, lambda_function, module):
     empty_lambda = module_config[1]
     ward_agg = single_document.get('ward')
     ward_buckets = ward_agg.get('buckets')
-    logging.info(" ward_buckets")
-    logging.info(ward_buckets)
+    
     for ward_bucket in ward_buckets:
         ward = ward_bucket.get('key')
         logging.info("ward info")
@@ -221,6 +220,8 @@ def transform_single(single_document, ward_map, date, lambda_function, module):
             ulb = ulb_bucket.get('key')
             region_agg = ulb_bucket.get('region')
             region_buckets = region_agg.get('buckets')
+            logging.info(" region_buckets")
+            logging.info(region_buckets)
             for region_bucket in region_buckets:
                 region = region_bucket.get('key')
                 if ward_map.get(get_key(ward,ulb)):
