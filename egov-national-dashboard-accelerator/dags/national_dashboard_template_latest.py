e460ee54e521d849c96f6a02872e165c90bce138
jitendra-eGov <100871838+jitendra-eGov@users.noreply.github.com>
2022-11-22 11:46:14 +0530

    Updated the string replce the special character

--- egov-national-dashboard-accelerator/dags/national_dashboard_template_latest.py
+++ egov-national-dashboard-accelerator/dags/national_dashboard_template_latest.py
@@ -214,7 +214,10 @@ def transform_single(single_document, ward_map, date, lambda_function, module):
     for ward_bucket in ward_buckets:
         ward = ward_bucket.get('key')
         ward= ward.replace("'", "")
-        ward=re.sub('[^;a-zA-z0-9 ' '  _ -]','',ward)
+        ward=re.sub('[^a-z0-9._()/&:,\\-  ]','',ward)
+        ward= ward.replace('[','')
+        ward= ward.replace(']','')
+        ward= ward.replace('|','')
         ulb_agg = ward_bucket.get('ulb')
         ulb_buckets = ulb_agg.get('buckets')
         for ulb_bucket in ulb_buckets:
