17e88f2810e40dd87780e4e1d5f504f2837e34bc
jitendra-eGov <100871838+jitendra-eGov@users.noreply.github.com>
2022-12-06 11:23:43 +0530

    Updated logs

--- egov-national-dashboard-accelerator/dags/queries/pt.py
+++ egov-national-dashboard-accelerator/dags/queries/pt.py
@@ -495,7 +495,7 @@ def extract_pt_collection_cess(metrics, region_bucket):
   collections =  []
   transactions = []
   logging.info("Data byUsageType")
-  logging.info(region_bucket.get('byUsageType'))
+  logging.info( region_bucket.get('byUsageType'))
   if region_bucket.get('byUsageType'):
     usage_buckets = region_bucket.get('byUsageType').get('buckets')
     for usage_bucket in usage_buckets:
