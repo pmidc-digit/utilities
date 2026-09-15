713f2d68fb5bba2ad842dfd0ac08e09ebf9e8c15
abhithakur2 <156669712+abhithakur2@users.noreply.github.com>
2026-09-15 18:19:48 +0530

    Update sw_amritsar_sql_test_dag.py
    update the postgress dag id

--- dag/sw_amritsar_sql_test_dag.py
+++ dag/sw_amritsar_sql_test_dag.py
@@ -14,7 +14,7 @@ with DAG(
 
     test_query = SQLExecuteQueryOperator(
         task_id="select_bills_summary",
-        conn_id="postgres_default",
+        conn_id="source_postgre",
         sql="""
             SELECT *
             FROM public.mv_sw_amritsar_bills_summary
